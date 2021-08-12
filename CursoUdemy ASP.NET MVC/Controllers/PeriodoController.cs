52bcc521dd5bf1570d71a47d5a633b427a7e107f
Alvaro Vian <alvarovian95@gmail.com>
2021-08-12 15:12:22 +0200

    Agregar y editar información (TERMINADO)

--- CursoUdemy ASP.NET MVC/Controllers/PeriodoController.cs
+++ CursoUdemy ASP.NET MVC/Controllers/PeriodoController.cs
@@ -17,8 +17,8 @@ namespace CursoUdemy_ASP.NET_MVC.Controllers
         public JsonResult listarPeriodo()
         {
             DataClasses1DataContext bd = new DataClasses1DataContext();
-           var lista = (bd.Periodo.Where(p => p.BHABILITADO.Equals(1))
-                .Select(p => new { p.IIDPERIODO, p.NOMBRE, FECHAINICIO = ((DateTime) p.FECHAINICIO).ToShortDateString(), FECHAFIN = ((DateTime) p.FECHAFIN).ToShortDateString() })).ToList();
+            var lista = (bd.Periodo.Where(p => p.BHABILITADO.Equals(1))
+                 .Select(p => new { p.IIDPERIODO, p.NOMBRE, FECHAINICIO = ((DateTime)p.FECHAINICIO).ToShortDateString(), FECHAFIN = ((DateTime)p.FECHAFIN).ToShortDateString() })).ToList();
 
             return Json(lista, JsonRequestBehavior.AllowGet);
         }
@@ -28,14 +28,81 @@ namespace CursoUdemy_ASP.NET_MVC.Controllers
         {
             DataClasses1DataContext bd = new DataClasses1DataContext();
             var lista = (bd.Periodo.Where(p => p.BHABILITADO.Equals(1) && p.NOMBRE.Contains(nombrePeriodo))
-                 .Select(p => new { 
-                     p.IIDPERIODO, 
-                     p.NOMBRE, 
-                     FECHAINICIO = ((DateTime)p.FECHAINICIO).ToShortDateString(), 
-                     FECHAFIN = ((DateTime)p.FECHAFIN).ToShortDateString() 
+                 .Select(p => new
+                 {
+                     p.IIDPERIODO,
+                     p.NOMBRE,
+                     FECHAINICIO = ((DateTime)p.FECHAINICIO).ToShortDateString(),
+                     FECHAFIN = ((DateTime)p.FECHAFIN).ToShortDateString()
                  })).ToList();
 
             return Json(lista, JsonRequestBehavior.AllowGet);
+        }
+
+        public int eliminar(Periodo periodo)
+        {
+            DataClasses1DataContext bd = new DataClasses1DataContext();
+            int nregistrosAfectados = 0;
+
+            try
+            {
+                int idperiodo = periodo.IIDPERIODO;
+                Periodo obj = bd.Periodo.Where(p => p.IIDPERIODO.Equals(idperiodo)).First();
+                obj.BHABILITADO = 0;
+                bd.SubmitChanges();
+                nregistrosAfectados = 1;
+            }
+            catch (Exception ex)
+            {
+                nregistrosAfectados = 0;
+            }
+            return nregistrosAfectados;
+        }
+
+        public JsonResult recuperarInformacion(int id)
+        {
+            DataClasses1DataContext bd = new DataClasses1DataContext();
+            var lista = bd.Periodo.Where(p => p.IIDPERIODO.Equals(id)).Select(p => new
+            {
+                p.IIDPERIODO,
+                p.NOMBRE,
+                FECHAINICIOCADENA = ((DateTime)p.FECHAINICIO).ToShortDateString(),
+                FECHAFINCADENA = ((DateTime)p.FECHAFIN).ToShortDateString()
+            }
+                );
+            return Json(lista, JsonRequestBehavior.AllowGet);
+        }
+
+        public int guardarDatos(Periodo oPeriodo)
+        {
+            DataClasses1DataContext bd = new DataClasses1DataContext();
+            int nregistrosAfectados = 0;
+            try
+            {
+                int idPeriodo = oPeriodo.IIDPERIODO;
+                if(idPeriodo >= 1)
+                {
+                    //EDITAR
+                    Periodo obj = bd.Periodo.Where(p => p.IIDPERIODO.Equals(idPeriodo)).First();
+                    obj.NOMBRE = oPeriodo.NOMBRE;
+                    obj.FECHAINICIO = oPeriodo.FECHAINICIO;
+                    obj.FECHAFIN = oPeriodo.FECHAFIN;
+                    bd.SubmitChanges();
+                    nregistrosAfectados = 1;
+                }
+                else
+                {
+                    //NUEVO
+                    bd.Periodo.InsertOnSubmit(oPeriodo);
+                    bd.SubmitChanges();
+                    nregistrosAfectados = 1;
+                }
+            }
+            catch(Exception ex)
+            {
+                nregistrosAfectados = 0;
+            }
+            return nregistrosAfectados;
         }
     }
 }
--- CursoUdemy ASP.NET MVC/Scripts/Periodo.js
+++ CursoUdemy ASP.NET MVC/Scripts/Periodo.js
@@ -1,4 +1,6 @@
-﻿$("#datepickerInicio").datepicker(
+﻿listar();
+
+$("#datepickerInicio").datepicker(
     {
         dateFormat: "dd/mm/yy",
         changeMonth: true,
@@ -50,8 +52,8 @@ function crearListado(data) {
         contenido += "<td>" + data[i].FECHAINICIO + "</td>";
         contenido += "<td>" + data[i].FECHAFIN + "</td>";
         contenido += "<td>";
-        contenido += "<button class = 'btn btn-primary' data-toggle='modal' data-target='#myModal'><i class='glyphicon glyphicon-edit'></i></button> ";
-        contenido += "<button class = 'btn btn-danger' data-toggle='modal' data-target='#myModal'><i class='glyphicon glyphicon-trash'></i></button> ";
+        contenido += "<button class = 'btn btn-primary' data-toggle='modal' data-target='#myModal' onclick='abrirModal(" + data[i].IIDPERIODO + ") '><i class='glyphicon glyphicon-edit'></i></button> ";
+        contenido += "<button class = 'btn btn-danger' onclick='Eliminar(" + data[i].IIDPERIODO + ")'><i class='glyphicon glyphicon-trash'></i></button> ";
         contenido += "</td>";
         contenido += "</tr>";
     }
@@ -62,4 +64,113 @@ function crearListado(data) {
     $("#tabla-periodo").dataTable({
         searching: false
     });
+}
+
+function borrarDatos() {
+    var controles = document.getElementsByClassName("borrar");
+    var ncontroles = controles.length;
+
+    for (var i = 0; i < ncontroles; i++) {
+        controles[i].value = "";
+    }
+}
+
+function datosObligatorios() {
+    var existo = true;
+    var controlesObligatorios = document.getElementsByClassName("obligatorio");
+    var ncontroles = controlesObligatorios.length;
+
+    for (var i = 0; i < ncontroles; i++) {
+        if (controlesObligatorios[i].value == "") {
+            existo = false;
+            controlesObligatorios[i].parentNode.classList.add("error");
+        }
+        else {
+            controlesObligatorios[i].parentNode.classList.remove("error");
+        }
+    }
+    return existo;
+}
+
+function Eliminar(id) {
+    var frm = new FormData();
+    frm.append("IIDPERIODO", id);
+    if (confirm("¿Desea realmente eliminarlo?") == 1) {
+        $.ajax({
+            type: "POST",
+            url: "Periodo/eliminar",
+            data: frm,
+            contentType: false,
+            processData: false,
+            success: function (data) {
+                if (data == 0) {
+                    alert("Ocurrio un error");
+                } else {
+                    listar();
+                    document.getElementById("btnCancelar").click();
+                }
+            }
+        });
+    }
+}
+
+function listar() {
+    $.get("Periodo/listarPeriodo", function (data) {
+        crearListado(data);
+    });
+}
+
+function abrirModal(id) {
+    var controlesObligatorios = document.getElementsByClassName("obligatorio");
+    var ncontroles = controlesObligatorios.length;
+
+    for (var i = 0; i < ncontroles; i++) {
+        controlesObligatorios[i].parentNode.classList.remove("error");
+    }
+
+    if (id == 0) {
+        borrarDatos();
+    } else {
+        $.get("Periodo/recuperarInformacion?id=" + id, function (data) {
+            document.getElementById("txtIdPeriodo").value = data[0].IIDPERIODO;
+            document.getElementById("txtNombrePopUp").value = data[0].NOMBRE;
+            document.getElementById("datepickerInicio").value = data[0].FECHAINICIOCADENA;
+            document.getElementById("datepickerFin").value = data[0].FECHAFINCADENA;
+        });
+
+    }
+}
+
+function Agregar() {
+    if (datosObligatorios() == true) {
+        var frm = new FormData();
+        var idperiodo = document.getElementById("txtIdPeriodo").value;
+        var nombre = document.getElementById("txtNombrePopUp").value;
+        var fechaInicio = document.getElementById("datepickerInicio").value;
+        var fechaFin = document.getElementById("datepickerFin").value;
+
+        frm.append("IIDPERIODO", idperiodo);
+        frm.append("NOMBRE", nombre);
+        frm.append("FECHAINICIO", fechaInicio);
+        frm.append("FECHAFIN", fechaFin);
+        frm.append("BHABILITADO", 1);
+
+        if (confirm("Desea realizar la operacion?") == 1) {
+            $.ajax({
+                type: "POST",
+                url: "Periodo/guardarDatos",
+                data: frm,
+                contentType: false,
+                processData: false,
+                success: function (data) {
+                    if (data != 0) {
+                        listar();
+                        document.getElementById("btnCancelar").click();
+                    } else {
+                        alert("Ocurrio un error");
+                    }
+                }
+            });
+        }
+    }
 }
